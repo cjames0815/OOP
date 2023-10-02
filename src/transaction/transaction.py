2aa2eb633e32a2258307a67552c8bd5a8d340d43
Cameron James <cjames0815@gmail.com>
2023-10-02 10:42:28 -0400

    FirstVersion of OOP

---
+++ src/transaction/transaction.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+class tranaction:
+    """The transaction abstract class defines methods that may be
+    implemented by an Account class.
+    """    
+    
+    @abstractmethod
+    def getBalance(self):
+        """Returns the current balance of this account
+        """        
+        
+        pass
+
+
+    @abstractmethod
+    def isEmpty(self):
+        """Checks if the balance for this account is zero
+        """        
+       
+    @abstractmethod
+    def credit(self, amount: int):
+        """Increases the balance of this account by the specified amount
+
+        Args:
+            amount (int): the amount to increase the balance by
+        """ 
+        pass       
+
+    @abstractmethod
+    def debit(self, amount: int):
+        """Decreases the balance of this account by the specified amount.
+
+        Args:
+            amount (int): the amount to decrease the balance by
+        """ 
+        pass       
